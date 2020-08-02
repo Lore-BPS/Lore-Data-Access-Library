2f6c8fb3786d89c13cb76e7d49541414e0883801
Maciej Jarzębkowski <maciej.jarzebkowski@gmail.com>
2020-08-03 00:54:55 +0200

    add workflow context

---
+++ Lore-Data-Access-Library/DataAccess/WorkflowContext.cs
@@ -0,0 +1,25 @@
+﻿using LoreDataAccessLibrary.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lore_Data_Access_Library.DataAccess
+{
+    public class WorkflowContext :DbContext
+    {
+        public WorkflowContext(DbContextOptions options): base(options) { }
+
+        public DbSet<LoreDataAccessLibrary.Model.Action> Action { get; set; }
+        public DbSet<LoreDataAccessLibrary.Model.ActionTypes> ActionTypes { get; set; }
+        public DbSet<LoreDataAccessLibrary.Model.BackendServers> BackendServers { get; set; }
+        public DbSet<LoreDataAccessLibrary.Model.Elements> Elements { get; set; }
+        public DbSet<LoreDataAccessLibrary.Model.Frontons> Frontons { get; set; }
+        public DbSet<LoreDataAccessLibrary.Model.HistoryElements> HistoryElements { get; set; }
+        public DbSet<LoreDataAccessLibrary.Model.Process> Process { get; set; }
+        public DbSet<LoreDataAccessLibrary.Model.Step> Steps { get; set; }
+
+
+
+    }
+}
